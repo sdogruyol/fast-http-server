0afab75ca3493cc8d7d542faa3e599225e4954d8
Mircea Ilie Ploscaru <tzekid2010@gmail.com>
2017-07-20 01:43:14 +0100

    fixed #1; added dir_listing toggle

--- src/fast-http-server.cr
+++ src/fast-http-server.cr
@@ -4,26 +4,30 @@ require "option_parser"
 class FastHttpServer < HTTP::StaticFileHandler
   property port
 
-  def initialize(@publicdir = "./")
+  def initialize(@publicdir = "./", @directory_listing = true)
     @port = 3000
+
     super
   end
 
   def call(context)
-    if context.request.path.not_nil! == "/"
-      index_path = @publicdir + "/index.html"
-      if File.exists?(index_path)
-        puts "#{index_path}"
-        redirect_to context, "/index.html"
+    if context.request.path.not_nil!
+      url = context.request.path
+      index_path = @publicdir + url.lchop + "index.html"
+      
+      if File.exists? index_path
+        redirect_to context, index_path
         return
       end
     end
+
     super
   end
 end
 
-
-directory : String  = "./"
+# Defaults
+directory  = "./"
+dir_listing = true
 
 BANNER =  <<-BANNER
 usage: fast-http-server [-h] [-d DIR] [port] 
@@ -34,13 +38,15 @@ positional arguments:
 optional arguments:
 BANNER
 
-
 at_exit do
   OptionParser.parse! do |parser|
     parser.banner = BANNER
 
     parser.on("-h", "--help", "show this help message and exit") { puts parser; exit }
     parser.on("-d DIR", "--directory DIR", "specify a directory") { |dir| directory = dir }
+    parser.on("-l LIST", "--listdir LIST", "enable/disable directory listing (true/false)") do |k|
+      dir_listing = false if k.downcase[0] == 'f'
+    end
 
     parser.invalid_option{ |opt| puts "Invalid option: #{opt}\n\n"; puts parser; exit }
     parser.missing_option{ |opt| puts "Missing option for #{opt}\n\n"; puts parser; exit }
@@ -48,7 +54,7 @@ at_exit do
 
   raise "Directory: '#{directory}' doesn't exist." unless Dir.exists?(directory)
 
-  fast_server = FastHttpServer.new directory
+  fast_server = FastHttpServer.new directory, dir_listing
 
   ARGV.map_with_index{ |arg, i|
     fast_server.port = ARGV[0].to_i?.nil? ? 3000 : ARGV[i].to_i
